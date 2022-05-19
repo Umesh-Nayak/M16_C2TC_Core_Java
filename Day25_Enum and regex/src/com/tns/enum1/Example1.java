c636d411d64094393d7fe4b6cd5688e7665f9f18
Admin <Admin@Umesh>
2022-05-19 15:56:19 +0530

    First Commit

---
+++ Day25_Enum and regex/src/com/tns/enum1/Example1.java
@@ -0,0 +1,14 @@
+package com.tns.enum1;
+enum Car
+{
+	Mercedes,BMW,Audi,Jaguar,Volkswagon;
+}
+public class Example1 {
+
+	public static void main(String[] args) {
+		Car Brand=Car.Jaguar;
+		System.out.println(Brand);
+
+	}
+
+}
---
+++ Day25_Enum and regex/src/com/tns/enum1/Example4.java
@@ -0,0 +1,22 @@
+package com.tns.enum1;
+interface Pizza
+{
+	void display();
+}
+enum size implements Pizza
+{
+	Regular,Medium,Large,ExtraLarge;
+		
+	@Override
+	public void display() {
+		System.out.println("Size is: "+this);
+	}
+}
+public class Example4 {
+
+	public static void main(String[] args) {
+		size.Regular.display();
+
+	}
+
+}
